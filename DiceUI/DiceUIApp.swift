7ff7bfc361670db13b6bfa34ffc1c1774b6e7707
vladys <wawladyslaw@gmail.com>
2024-12-08 11:02:48 +0200

    Initial Commit

---
+++ DiceUI/DiceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceUIApp.swift
+//  DiceUI
+//
+//  Created by Vladyslav Tarabunin on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiceUITests/DiceUITests.swift
@@ -0,0 +1,17 @@
+//
+//  DiceUITests.swift
+//  DiceUITests
+//
+//  Created by Vladyslav Tarabunin on 08/12/2024.
+//
+
+import Testing
+@testable import DiceUI
+
+struct DiceUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
